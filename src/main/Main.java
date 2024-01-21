8d0d1208dda1014cb3de6325a93e06bbd1932383
ryeongeunkim <ryeongeun.gim@gmail.com>
2024-01-21 18:10:05 +0900

    add: 문자 찾기 함수 추가

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,19 @@ package  src.main;
 
 import src.main.java.algorithms.strings.StringManipulation;
 
+import java.util.Scanner;
+
 public class Main {
+    public void solution() {
+
+    }
+
     public static void main(String[] args) {
-        // 문자열 조작 예제
-        StringManipulation.stringManipulationExample();
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt((0));
+        System.out.println(StringManipulation.stringManipulationExample(str, c));
+//        System.out.println(T.solution(str, c));
     }
 }
--- src/main/java/algorithms/strings/StringManipulation.java
+++ src/main/java/algorithms/strings/StringManipulation.java
@@ -1,20 +1,16 @@
 package src.main.java.algorithms.strings;
 
 public class StringManipulation {
-    // 문자열 조작 관련 함수
-    public static void stringManipulationExample() {
-        String inputString = "Hello, World!";
+    // 문자 찾기
+    public static int stringManipulationExample(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
 
-        // 문자열의 길이 출력
-        int length = inputString.length();
-        System.out.println("문자열의 길이: " + length);
+        for (char x : str.toCharArray()) {
+            if (x == t) answer++;
+        }
 
-        // 대문자로 변환하여 출력
-        String upperCaseString = inputString.toUpperCase();
-        System.out.println("대문자로 변환: " + upperCaseString);
-
-        // 소문자로 변환하여 출력
-        String lowerCaseString = inputString.toLowerCase();
-        System.out.println("소문자로 변환: " + lowerCaseString);
+        return answer;
     }
 }
